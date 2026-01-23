fe439990f0e4a1a1ef09038305756ff5681dc027
Kauan Lima <k.alves1427@gmail.com>
2026-01-23 17:57:57 -0300

    Upgrade do Next.js

---
+++ .next/dev/static/chunks/Downloads_next-js-portfolio-project_pages__error_2da965e7._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__967511bb._.js",
+  "static/chunks/17390_react_3b8351e2._.js",
+  "static/chunks/17390_react-dom_cjs_react-dom_development_6a389a2f.js",
+  "static/chunks/17390_react-dom_cfd0371d._.js",
+  "static/chunks/_89ac76f6._.js",
+  "static/chunks/[root-of-the-server]__e100ca11._.js"
+],
+    source: "entry"
+});
